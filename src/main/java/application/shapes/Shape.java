14d7c9ff2ba458d5b67abe8b3b42692eaafecf5c
DEVO27 <A.G-Zoe@outlook.com>
2022-10-21 21:36:58 -0500

    Created base class implementation

--- src/main/java/application/shapes/Shape.java
+++ src/main/java/application/shapes/Shape.java
@@ -1,2 +1,36 @@
-package application.Shapes;public class Shape {
+package application.shapes;
+
+public abstract class Shape {
+    private Colors color;
+    private int xCoordinate;
+    private int yCoordinate;
+
+    public int getXCoordinate() {
+        return xCoordinate;
+    }
+
+    public void setXCoordinate(int xCoordinate) {
+        this.xCoordinate = xCoordinate;
+    }
+
+    public int getYCoordinate() {
+        return yCoordinate;
+    }
+
+    public void setYCoordinate(int yCoordinate) {
+        this.yCoordinate = yCoordinate;
+    }
+
+    public Colors getColor() {
+        return color;
+    }
+
+    public void setColor(Colors color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Color: %s, Origin: (%d %d),".formatted(getColor().toString(), getXCoordinate(), getYCoordinate());
+    }
 }
