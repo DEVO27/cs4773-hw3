f2be50edfc90a7c1bfd72fe65c8244b4d85a39d0
DEVO27 <A.G-Zoe@outlook.com>
2022-10-21 21:35:10 -0500

    Added Rectangle implementation

--- src/main/java/application/shapes/Rectangle.java
+++ src/main/java/application/shapes/Rectangle.java
@@ -1,2 +1,35 @@
-package application.Shapes;public class Rectangle {
+package application.shapes;
+
+public class Rectangle extends Shape {
+    private int width;
+    private int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+        super.setXCoordinate(0);
+        super.setYCoordinate(0);
+        super.setColor(Colors.Red);
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle, %s Width: %d, Height: %d".formatted(super.toString(), getWidth(), getHeight());
+    }
 }
