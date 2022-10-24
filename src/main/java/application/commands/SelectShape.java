55f9e7986f5d7b231b177905e3d20809276f4968
DEVO27 <A.G-Zoe@outlook.com>
2022-10-24 18:44:47 -0500

    Created and Implemented undo class

--- src/main/java/application/commands/SelectShape.java
+++ src/main/java/application/commands/SelectShape.java
@@ -2,36 +2,28 @@ package application.commands;
 
 import java.util.Stack;
 
-public class Scene implements ICommand {
+public class SelectShape implements ICommand {
     private Stack<Memento> indexStack = new Stack<>();
     private Boolean active;
-    /*private List<Shape> shapes;
-    private Stack previousIndex;
-    private Shape curShape; */
     private int index;
 
-    public Scene(int index) {
-        this.index = index - 1;
-        /*this.previousIndex = new Stack<>();
-        this.shapes = new ArrayList<>(); */
+    public SelectShape(int index) {
+        this.index = index;
     }
 
     @Override
     public void execute() throws IndexOutOfBoundsException {
         saveToMemento();
         setIndex(index);
-        /**previousIndex.push(index);
-        setCurShape(shapes.get(index)); ***/
     }
 
     @Override
     public void unExecute() {
-        getMemento();
-        /*if (!getPreviousIndex().isEmpty()) {
-            setCurShape(shapes.get((int) getPreviousIndex().pop()));
+        if (!indexStack.isEmpty()){
+            getMemento();
         } else {
             System.out.println("No Shape");
-        } */
+        }
     }
 
     public void saveToMemento() {
@@ -54,22 +46,6 @@ public class Scene implements ICommand {
         }
     }
 
-    /**public List<Shape> getShapes() {
-        return shapes;
-    }
-
-    public void setShapes(ArrayList<Shape> shapes) {
-        this.shapes = shapes;
-    }
-
-    public Shape getCurShape() {
-        return curShape;
-    }
-
-    public void setCurShape(Shape curShape) {
-        this.curShape = curShape;
-    } **/
-
     public int getIndex() {
         return index;
     }
@@ -78,23 +54,19 @@ public class Scene implements ICommand {
         this.index = index;
     }
 
-    /***public Stack getPreviousIndex() {
-        return previousIndex;
-    }
-
-    public void setPreviousIndex(Stack previousIndex) {
-        this.previousIndex = previousIndex;
-    }
-
-    public void setShapes(List<Shape> shapes) {
-        this.shapes = shapes;
-    } **/
-
     public Boolean getActive() {
         return active;
     }
 
     public void setActive(Boolean active) {
         this.active = active;
+    }
+
+    public Stack<Memento> getIndexStack() {
+        return indexStack;
+    }
+
+    public void setIndexStack(Stack<Memento> indexStack) {
+        this.indexStack = indexStack;
     }
 }
--- src/main/java/application/commands/UndoCommand.java
+++ src/main/java/application/commands/UndoCommand.java
@@ -1,41 +1,50 @@
 package application.commands;
 
-import application.commands.loader.Invoker;
+import application.commands.interpreter.InvokeCommands;
+import application.model.Scene;
 
 public class UndoCommand implements ICommand {
-    private final SelectShape selectShape;
+    private final Scene sceneShape;
     private final String command;
+    private final SelectShape selectShape;
 
-    public UndoCommand(String command, SelectShape selectShape) {
-        this.selectShape = selectShape;
+    public UndoCommand(String command, Scene sceneShape, SelectShape selectShape) {
+        this.sceneShape = sceneShape;
         this.command = command;
+        this.selectShape = selectShape;
     }
 
-    private void select() {
-        int index = selectShape.getShapes().size() - 1;
+    private void choice() {
+        int index = sceneShape.getSavedShapes().size() - 1;
+        int curIndex = selectShape.getIndex();
         switch (command) {
-            case "CREATE RECTANGLE", "CREATE CIRCLE" -> selectShape.getShapes().remove(index);
-            case "MOVE" -> new Invoker(new MoveShape(selectShape.getCurShape())).undoMove();
-            case "SELECT" -> new Invoker(new SelectShape(selectShape.getIndex())).undoSelect();
-            case "COLOR" -> new Invoker(new ChangeColor(selectShape.getCurShape())).undoColor();
+            case "CREATE RECTANGLE", "CREATE CIRCLE" -> sceneShape.getSavedShapes().remove(index);
+            case "MOVE" -> new InvokeCommands(new MoveShape(sceneShape.getShape(curIndex))).undoMove();
+            case "SELECT" -> new InvokeCommands(new SelectShape(curIndex)).undoSelect();
+            case "COLOR" -> new InvokeCommands(new ChangeColor(sceneShape.getShape(curIndex))).undoColor();
+            case "DELETE" -> new InvokeCommands(new DeleteShape(curIndex, sceneShape)).undoDeleteShape();
         }
     }
 
 
     @Override
     public void execute() {
-        select();
+        choice();
     }
 
     @Override
     public void unExecute() {
     }
 
-    public SelectShape getSelectShape() {
-        return selectShape;
+    public Scene getSceneShape() {
+        return sceneShape;
     }
 
     public String getCommand() {
         return command;
+    }
+
+    public SelectShape getSelectShape() {
+        return selectShape;
     }
 }
