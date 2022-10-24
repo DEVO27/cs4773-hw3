a1780a6fe6b23c0787658d74662064bcc68dba35
DEVO27 <A.G-Zoe@outlook.com>
2022-10-24 18:43:04 -0500

    Implemented Delete Class

--- src/main/java/application/commands/DeleteShape.java
+++ src/main/java/application/commands/DeleteShape.java
@@ -1,2 +1,44 @@
-package application.commands;public class DeleteShape {
+package application.commands;
+
+import application.model.Scene;
+
+public class DeleteShape implements ICommand {
+    private Scene scene;
+    private int index;
+
+    public DeleteShape(int index, Scene scene) {
+        this.index = index;
+        this.scene = scene;
+    }
+
+    @Override
+    public void execute() {
+        scene.saveToMemento();
+        scene.getSavedShapes().remove(index);
+    }
+
+    @Override
+    public void unExecute() {
+        if (!scene.getMementoStack().isEmpty()) {
+            scene.getMemento();
+        } else {
+            System.out.println("no shape selected");
+        }
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
+
+    public void setScene(Scene scene) {
+        this.scene = scene;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
 }
