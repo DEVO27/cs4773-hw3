bc31504b87c0a86b6c80deb2567ffbe63f02ac99
DEVO27 <A.G-Zoe@outlook.com>
2022-10-24 18:49:12 -0500

    Updated entry class

--- src/main/java/application/model/Scene.java
+++ src/main/java/application/model/Scene.java
@@ -6,36 +6,63 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-public class CareTaker {
-    List<Shape> savedShapes = new ArrayList<>();
-    Stack<Memento> mementoStack = new Stack<>();
+public class Scene {
+    private List<Shape> savedShapes = new ArrayList<>();
+    private Stack<Memento> mementoStack = new Stack<>();
+    private Shape curShape;
 
     public void addShape(Shape shape) {
         savedShapes.add(shape);
     }
 
+    public void setCurShape(Shape curShape) {
+        this.curShape = curShape;
+    }
 
     public Shape getShape(int index) {
         return savedShapes.get(index);
     }
 
     public void saveToMemento() {
-        mementoStack.push(new CareTaker.Memento(savedShapes));
+        mementoStack.push(new Scene.Memento(getSavedShapes()));
     }
 
-    public void getMementoStack() {
-        savedShapes = mementoStack.pop().getShapeList();
+    public Stack<Memento> getMementoStack() {
+        return mementoStack;
+    }
+
+    public void getMemento() {
+        setSavedShapes(mementoStack.pop().shapeList);
+    }
+
+    public void setMementoStack(Stack<Memento> mementoStack) {
+        this.mementoStack = mementoStack;
+    }
+
+    public Shape getCurShape() {
+        return curShape;
     }
 
     public void setShape(Shape shape, int index) {
         savedShapes.set(index, shape);
     }
 
+    public List<Shape> getSavedShapes() {
+        return savedShapes;
+    }
+
+    public void setSavedShapes(List<Shape> savedShapes) {
+        this.savedShapes = savedShapes;
+    }
+
+
+
     public class Memento {
         List<Shape> shapeList;
 
         public Memento(List<Shape> shapeList) {
-            this.shapeList = shapeList;
+            this.shapeList = new ArrayList<>();
+            this.shapeList.addAll(shapeList);
         }
 
         public List<Shape> getShapeList() {
@@ -44,7 +71,4 @@ public class CareTaker {
     }
 
 
-    public void setMementoStack(Stack<Memento> mementoStack) {
-        this.mementoStack = mementoStack;
-    }
 }
