bc57965095a7f5998c5fa1a68e622d67f7e92e79
Ker Xiang Teh <kerxiangteh@gmail.com>
2022-10-26 16:24:21 -0500

    Moved the boolean check from ProcessCommands.moveShape to MoveShape.execute().

--- src/main/java/application/commands/MoveShape.java
+++ src/main/java/application/commands/MoveShape.java
@@ -1,14 +1,21 @@
 package application.commands;
 
+import application.model.Scene;
 import application.shape.Shape;
 
 public class MoveShape implements ICommand {
+    private Scene scene;
+    private SelectShape selectShape;
     private Shape shape;
+    private int index;
     private int x;
     private int y;
 
-    public MoveShape(Shape shape, int x, int y) {
-        this.shape = shape;
+    public MoveShape(SelectShape selected, Scene scene, int x, int y) {
+        this.scene = scene;
+        this.selectShape = selected;
+        this.index = selectShape.getIndex();
+        this.shape = scene.getShape(index);
         this.x = x;
         this.y = y;
     }
@@ -19,9 +26,15 @@ public class MoveShape implements ICommand {
 
     @Override
     public void execute() {
-        shape.saveToMemento();
-        shape.setXCoordinate(getX());
-        shape.setYCoordinate(getY());
+        if (Boolean.TRUE.equals(selectShape.getActive())) {
+            shape.saveToMemento();
+            shape.setXCoordinate(getX());
+            shape.setYCoordinate(getY());
+            scene.setShape(scene.getShape(index), index);
+        }
+        else {
+            System.out.println("No shape selected");
+        }
     }
 
     @Override
