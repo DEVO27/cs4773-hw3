7582397df9e767430458dc490751e106c15663a9
DEVO27 <A.G-Zoe@outlook.com>
2022-10-26 19:49:55 -0500

    Final Edits

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -4,6 +4,6 @@ import application.commands.operations.LoadCommands;
 
 public class App {
     public static void main(String[] args) {
-        new LoadCommands().readFile(args[1]);
+        new LoadCommands().readFile(args[0]);
     }
 }
