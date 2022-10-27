17dbdb6b51c8acfbac4ba5bb2e371f6b8124dca2
DEVO27 <A.G-Zoe@outlook.com>
2022-10-26 19:50:57 -0500

    Last Edit

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -2,6 +2,9 @@ package application;
 
 import application.commands.operations.LoadCommands;
 
+/**
+ * Beginning of the application
+ */
 public class App {
     public static void main(String[] args) {
         new LoadCommands().readFile(args[0]);
